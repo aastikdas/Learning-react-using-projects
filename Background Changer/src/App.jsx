828d3adebb49b2d95e89d87ba15334a03ef8384f
aastikdas <aastikdas126@gmail.com>
2025-03-23 20:49:14 +0530

    updated Readme

--- Background Changer/src/App.jsx
+++ Background Changer/src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Footer from '../components/footer'
-import './App.css'
-import './index.css'
-
 function App() {
-
   return (
-    <>
-      
+    <>      
       <Footer/>
     </>
   )
